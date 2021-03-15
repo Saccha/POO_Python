43ecad116d66c972bf5f1143d0e1aea0cd7c1669
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-03-15 10:05:27 -0300

    Update Excecoes.py

--- Excecoes.py
+++ Excecoes.py
@@ -1,5 +1,5 @@
 """
-Identifique as partes problemáticas no código
+Questão 1:Identifique as partes problemáticas no código
 a seguir e reescreva-o tratando as possı́veis exceções.
 """
 
@@ -14,3 +14,24 @@ except ZeroDivisionError:
 	print('Erro: o valor de y não pode ser zero!')
 except:
 	print('Um erro inesperado ocorreu!')
+	
+"""	
+Questão 2: Modifique o código da Questão 1 
+para que sejam solicitados novos valores caso uma exceção ocorra.
+"""
+
+ocorreuErro = True
+while ocorreuErro:
+	try:
+		x = int(input('Primeiro valor: '))
+		y = int(input('Segundo valor: '))
+		z = x / y
+		print('O resultado da divisão é:', z)
+	except ValueError:
+		print('Erro: os valores de x e y devem ser inteiros!')
+	except ZeroDivisionError:
+		print('Erro: o valor de y não pode ser zero!')
+	except:
+		print('Um erro inesperado ocorreu!')
+	else:
+		ocorreuErro = False
